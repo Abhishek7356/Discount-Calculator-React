e747034dcb5ec0d900fe1feefba00e5ade763570
Abhishek7356 <ksabhishek32@gmail.com>
2023-10-07 15:41:59 +0530

    made a small change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -37,10 +37,10 @@ function Main() {
                 <div className="container">
                     <div className="containerDiv">
                         <div>
-                            <motion.h1 transition={{ duration: '.20' }} initial={{ x: '-600px', opacity: '0' }}
+                            <motion.h1 transition={{ duration: '.20' }} initial={{ x: '-100%', opacity: '0' }}
                                 whileInView={{ x: '0px', opacity: '1' }}
                                 viewport={{ once: true }} style={{ fontFamily: 'Josefin+Sans' }} className='heading'>Discount Calculator <br /> Online</motion.h1>
-                            <motion.p transition={{ duration: '.20', delay: '.10' }} initial={{ x: '-600px', opacity: '0' }}
+                            <motion.p transition={{ duration: '.20', delay: '.10' }} initial={{ x: '-100%', opacity: '0' }}
                                 whileInView={{ x: '0px', opacity: '1' }}
                                 viewport={{ once: true }} className='desc'>Calculate The Discount Amount In A Few Seconds</motion.p>
                         </div>
@@ -50,38 +50,38 @@ function Main() {
                     </div>
                 </div>
             </div>
-            <motion.div transition={{ duration: '.20', delay: '.20' }} initial={{ x: '-1000px', opacity: '0' }}
+            <motion.div transition={{ duration: '.20', delay: '.20' }} initial={{ x: '-100%', opacity: '0' }}
                 whileInView={{ x: '0px', opacity: '1' }}
                 viewport={{ once: true }} className="main container shadow">
-                <motion.h1 transition={{ duration: '.20', delay: '.10' }} initial={{ y: '50px', opacity: '0' }}
-                    whileInView={{ y: '-20px', opacity: '1' }}
+                <motion.h1 transition={{ duration: '.20', delay: '.20' }} initial={{ x: '-100%', opacity: '0' }}
+                    whileInView={{ x: '0px', opacity: '1' }}
                     viewport={{ once: true }} className='discountCalculator'>Instant Discount Calculator</motion.h1>
-                <motion.p transition={{ duration: '.20', delay: '.10' }} initial={{ y: '50px', opacity: '0' }}
-                    whileInView={{ y: '-20px', opacity: '1' }}
+                <motion.p transition={{ duration: '.20', delay: '.20' }} initial={{ x: '-100%', opacity: '0' }}
+                    whileInView={{ x: '0px', opacity: '1' }}
                     viewport={{ once: true }} className="description">Use the discount calculator to find out the exact amount you will save!</motion.p>
                 <div className="discount">
                     <div className='input'>
-                        <motion.div transition={{ duration: '.20' }} initial={{ y: '50px' }}
-                            whileInView={{ y: '-20px' }}
+                        <motion.div transition={{ duration: '.20' }} initial={{ x: '-100%' }}
+                            whileInView={{ x: '0px' }}
                             viewport={{ once: true }} className='details'>
                             <label className='label'>Amount (₹)</label>
                             <input onChange={(e) => handleAmount(e)} value={amount} className='inputamout form-control' type='number' placeholder='Enter Amount' />
                         </motion.div>
-                        <motion.div transition={{ duration: '.20' }} initial={{ y: '50px' }}
-                            whileInView={{ y: '-20px' }}
+                        <motion.div transition={{ duration: '.20' }} initial={{ x: '-100%' }}
+                            whileInView={{ x: '0px' }}
                             viewport={{ once: true }} className='details'>
                             <label className='label' >Discount applied (%)</label>
                             <input onChange={(e) => handleRate(e)} value={rate} className='inputamout form-control' type='number' placeholder='Enter discount value in %' />
                         </motion.div>
-                        <motion.div transition={{ duration: '.20' }} initial={{ y: '50px' }}
-                            whileInView={{ y: '-20px' }}
+                        <motion.div transition={{ duration: '.20' }} initial={{ x: '-100%' }}
+                            whileInView={{ x: '0px' }}
                             viewport={{ once: true }}>
                             <Button onClick={handleDiscount} variant="contained" className='ms-2'>Submit</Button>
                             <Button onClick={handleReset} variant="contained" className='ms-2'>Reset</Button>
                         </motion.div>
                     </div>
-                    <motion.div transition={{ duration: '.20' }} initial={{ y: '50px' }}
-                        whileInView={{ y: '-20px' }}
+                    <motion.div transition={{ duration: '.20' }} initial={{ x: '400px' }}
+                        whileInView={{ x: '10px' }}
                         viewport={{ once: true }} className="resultBox shadow">
                         <h1 className='discountCalculator2'>Amount Paid</h1>
                         <h3 className='priceTag'>New Price</h3>
